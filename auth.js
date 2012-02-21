80c999b54a7e5edaded96d3a7269bf8efafcddbd
Simon Murtha-Smith <simon@murtha-smith.com>
2012-02-21 14:41:18 -0800

    commit the extension for now and link to ?extenion=true

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 module.exports = {
   handler : function (host, apiKeys, done, req, res) {
     if(req.query.done) return done();
+    if(req.query.extension) return res.download(__dirname + '/extension.crx');
     res.sendfile(__dirname + '/auth.html');
   }
 }
