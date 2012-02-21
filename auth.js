b54a82b028905611d7d020da8acfe4bd223410c5
Simon Murtha-Smith <simon@murtha-smith.com>
2012-02-21 14:10:20 -0800

    working with latest local locker updates to registry

--- auth.js
+++ auth.js
@@ -1,9 +1,6 @@
 module.exports = {
   handler : function (host, apiKeys, done, req, res) {
-    if(!req.query.done) {
-      res.sendfile(__dirname + '/auth.html');
-    } else {
-      res.send('wahoooo!');
-    }
+    if(req.query.done) return done();
+    res.sendfile(__dirname + '/auth.html');
   }
 }
